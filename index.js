261a1808fc01e7cbc4fd635a7c9f39553f17e2b5
Eduardo Henrique Meurer <eduardo@Laptop-de-Eduardo.local>
2024-09-30 20:02:02 -0300

    feat: aula 2 de javascript

--- index.js
+++
@@ -1,8 +0,0 @@
-function exibirMensagem() {
-    return console.log("Retornou uma mensagem");
-}
-
-function getMessage() {
-    return alert("Olá");
-}
-exibirMensagem();
---
+++ script.js
@@ -0,0 +1,52 @@
+var time = "Palmeiras";
+let idade = "110";
+const cidade = "São Paulo";
+
+console.log("Nome:", time);
+console.log("Idade:", idade);
+console.log("Localização:", cidade);
+
+// Exemplo de objeto
+const pessoa = {
+    nome: "Jonerval",
+    idade: "32",
+    sexo: "Masculino",
+    profissao: "Ladrão",
+}
+
+console.log(pessoa);
+
+let gato = {
+    nome: "Panda",
+    idade: "8",
+    vacinado: "false",
+}
+
+gato.nome = "Pandinha";
+gato.vacinado = "true";
+
+console.log(gato);
+
+//  Concatenação de Strings
+let nome = "Jonerval";
+let message = "Eu " + nome + " sou ladrão, tenho 32 anos e não aguento trabalhar mais não";
+
+console.log(message);
+
+// Operações com Numbers
+let a = 25;
+let b = 30;
+
+let total = a + b;
+console.log("O total é: ", total);
+
+// Conversão para String
+let turma = 1;
+let turmaFormatado = String(turma);
+console.log(turmaFormatado);
+
+// Conversão para Number
+let telefone = "4299999999";
+let telefoneFormatado = Number(telefone);
+console.log(telefoneFormatado);
+
