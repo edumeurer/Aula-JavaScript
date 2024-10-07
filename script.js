d3bb2dde89315504b5cb03a31c00392acf3f8680
Eduardo Henrique Meurer <eduardo@Laptop-de-Eduardo.local>
2024-10-07 20:17:47 -0300

    feat: trabalho calculadora

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-// Função Simples (Declarativa)
-function saudacao() {
-    return console.log("Olá mundo");
+// Trabalho Calculadora
+
+function calculadora(a, b, condicao) {
+    if (condicao == 'soma') {
+        return a + b;
+    } else if (condicao == 'subtracao') {
+        return a - b;
+    } else if (condicao == 'multiplicacao') {
+        return a * b;
+    } else if (condicao == 'divisao') {
+            return a / b;
+    } else {
+        return "Operação inválida";
+    }
 }
-
-// Função com Parâmetros
-function soma(valorA, valorB) {
-    const totalSoma = valorA + valorB;
-    return console.log(totalSoma);
-}
-
-// Funcão Anônima
-const multiplicar = function (valorA, valorB) {
-    const total = valorA * valorB;
-    return console.log(total);
-}
-
-// Arrow Function
-const dividir = (a, b) => a / b;
-
-// Função com valor padrão de parãmetro
-function exponencial(base, expoente = 2) {
-    const exp = base ** expoente;
-    return console.log(exp);
-}
-
-// Invocando as Funções
-saudacao();
-soma(10, 5);
-multiplicar(5, 4);
-console.log(dividir(10, 2));
-exponencial(3);
-exponencial(3, 9);
-
-// Função de Callback
-function inicializarAplicacao() {
-    saudacao();
-    soma(10, 5);
-    multiplicar(5, 4);
-    exponencial(3);
-    exponencial(3, 9);
-}
-inicializarAplicacao();
+console.log(calculadora(10, 2, 'soma'));
+console.log(calculadora(10, 2, 'subtracao'));
+console.log(calculadora(10, 2, 'multiplicacao'));
+console.log(calculadora(10, 2, 'divisao'));
+console.log(calculadora(10, 2, 'modulo'));
