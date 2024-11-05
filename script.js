3126bf9f64ab39729437850fd13c2bfda3f9c5ef
edumeurer <edusep290893@gmail.com>
2024-11-05 17:23:59 -0300

    feat: trabalho api rick and morty

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-let arrayPokemons;
-function getPokemon() {
-    fetch (
-        "https://pokeapi.co/api/v2/pokemon",
-        {
-            method: "GET"
-        }
-    )
+let arrayCharacters;
+function getCharacters() {
+    fetch("https://rickandmortyapi.com/api/character/?page=19", {
+        method: "GET"
+    })
     .then((response) => response.json())
     .then((data) => {
-        arrayPokemons = data.results;
-        appendData(arrayPokemons);
+        arrayCharacters = data.results;
+        appendData(arrayCharacters);
     })
     .catch((error) => {
         console.error("Erro", error);
-    })
+    });
 }
-function appendData(pokemons) {
+function appendData(characters) {
     let place = document.querySelector("#data-output");
     let output = "";
 
-    for (let pokemon of pokemons) {
+    for (let character of characters) {
         output += `
             <tr>
-                <td>${pokemon.name}</td>
-                <td>${pokemon.url}</td>
+                <td><img src="${character.image}" alt="${character.name}" width="50" height="50"></td>
+                <td>${character.name}</td>
+                <td>${character.status}</td>
+                <td>${character.species}${character.type ? ` - ${character.type}` : ''}</td>
+                <td>${character.origin.name}</td>
+                <td>${character.location.name}</td>
             </tr>
-        `
+        `;
     }
     place.innerHTML = output;
 }
-getPokemon();
+getCharacters();
